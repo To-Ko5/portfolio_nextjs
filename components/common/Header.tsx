2af3d2aa002066686512a96a6985be108c755800
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-03-30 22:50:22 +0900

    initial commit

---
+++ components/common/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="bg-gray-50 dark:bg-gray-900  p-4">
+      <h1 className="text-4xl">title</h1>
+    </header>
+  )
+}
+
+export default Header
