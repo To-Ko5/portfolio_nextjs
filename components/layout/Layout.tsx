356348774b984c7c32045923b8be389c002b5928
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-03-31 08:11:38 +0900

    Add: Sidebarを読み込む

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,10 +1,13 @@
 import React, { FC } from 'react'
+
 import Header from '../common/Header'
+import Sidebar from '../common/Sidebar'
 
 const Layout: FC = ({ children }) => {
   return (
     <React.Fragment>
       <Header />
+      <Sidebar />
       <main>{children}</main>
     </React.Fragment>
   )
