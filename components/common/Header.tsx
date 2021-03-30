d23d1aa81f6a83086eda8ad90a8230d0aafdae2b
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-03-31 08:11:54 +0900

    Add: menuiconを追加

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -3,7 +3,25 @@ import SwitchingTheme from '../common/SwitchingTheme'
 const Header = () => {
   return (
     <header className="bg-gray-50 dark:bg-dark-black shadow p-4 flex justify-between">
-      <h1 className="text-4xl">title</h1>
+      <div className="flex items-center">
+        <button className="w-8 mr-8">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
+        <h1 className="text-4xl font-medium a">C-Portfolio</h1>
+      </div>
+
       <SwitchingTheme />
     </header>
   )
