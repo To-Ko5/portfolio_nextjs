72ae4f9b94431e7b19c245a0c95103f4c755613a
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-03-30 22:50:06 +0900

    Fix: 設定を変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {}
+    fontFamily: {
+      robot: ['Roboto', 'sans-serif']
+    }
   },
   variants: {
     extend: {}
