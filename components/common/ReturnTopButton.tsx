2b18dff3510dd97b7104e115c259816072a7b139
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-04-02 07:44:46 +0900

    Add: styleを追加

--- components/common/ReturnTopButton.tsx
+++ components/common/ReturnTopButton.tsx
@@ -1,5 +1,24 @@
 const ReturnTopButton = () => {
-  return <div></div>
+  return (
+    <button className="fixed bottom-3.5 right-3.5 w-14 h-14 p-2 text-center rounded-full bg-gray-50 dark:bg-dark-black shadow-lg outline-none border-0">
+      <span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          className=""
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M5 15l7-7 7 7"
+          />
+        </svg>
+      </span>
+    </button>
+  )
 }
 
 export default ReturnTopButton
