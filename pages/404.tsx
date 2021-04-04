d2d6573af60e7bd750a31adbfd2051f015bb5da5
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-04-04 09:26:34 +0900

    initial commit

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+import Layout from '../components/layout/Layout'
+
+const Page404 = () => {
+  return (
+    <Layout>
+      <main className="">404</main>
+    </Layout>
+  )
+}
+
+export default Page404
