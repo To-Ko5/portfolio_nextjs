836f1cb4c6e7276ee26d5e04320381045a5d7476
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-04-04 16:07:45 +0900

    Add: propsの値を整形

--- components/posts/PostCard.tsx
+++ components/posts/PostCard.tsx
@@ -1,22 +1,55 @@
 import Image from 'next/image'
 
+import {
+  PostImage,
+  PostArticle,
+  PostCategory,
+  PostTags
+} from '../../types/PostType'
+
 const PostCard = ({ id, post }) => {
-  // tslint:disable-next-line: prefer-template
-  const imageSrc = 'https://' + post.image.fields.file.url
-  const imageAlt = post.image.fields.title as string
-  const imageWidth = post.image.fields.file.details.image.width as number
-  const imageHeight = post.image.fields.file.details.image.height as number
-  console.log(post)
+  const image: PostImage = {
+    src: `https:${post.image.fields.file.url}`,
+    alt: post.image.fields.title,
+    width: post.image.fields.file.details.image.width,
+    height: post.image.fields.file.details.image.height
+  }
+
+  const article: Omit<PostArticle, 'content'> = {
+    title: post.title,
+    subTitle: post.subtitle
+  }
+
+  const category: PostCategory = {
+    id: post.category.sys.id,
+    name: post.category.fields.name
+  }
+
+  const tags: PostTags[] = []
+  post.tag.map((e) => {
+    tags.push({ id: e.sys.id, name: e.fields.name })
+  })
+
   return (
-    <div>
-      {id},
-      <div className="w-3/12">
+    <div className="bg-gray-50 dark:bg-dark-black shadow-md">
+      <div className="">
         <Image
-          src={imageSrc}
-          alt={imageAlt}
-          width={imageWidth}
-          height={imageHeight}
+          src={image.src}
+          alt={image.alt}
+          width={image.width}
+          height={image.height}
         />
+      </div>
+      <p>{article.title}</p>
+      <p>{article.subTitle}</p>
+      <div>{/* {category.id},{category.name} */}</div>
+      <div>
+        {tags &&
+          tags.map((tag) => (
+            <div key={tag.id}>
+              {tag.id},{tag.name}
+            </div>
+          ))}
       </div>
     </div>
   )
