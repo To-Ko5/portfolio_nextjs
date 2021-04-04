f8fc8e00d75ab5ea7df4083af42650b8eadd6227
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-04-04 10:29:53 +0900

    Add: 画像の設定

--- components/posts/PostCard.tsx
+++ components/posts/PostCard.tsx
@@ -1,8 +1,23 @@
+import Image from 'next/image'
+
 const PostCard = ({ id, post }) => {
+  // tslint:disable-next-line: prefer-template
+  const imageSrc = 'https://' + post.image.fields.file.url
+  const imageAlt = post.image.fields.title as string
+  const imageWidth = post.image.fields.file.details.image.width as number
+  const imageHeight = post.image.fields.file.details.image.height as number
   console.log(post)
   return (
     <div>
-      {id},{post.title}
+      {id},
+      <div className="w-3/12">
+        <Image
+          src={imageSrc}
+          alt={imageAlt}
+          width={imageWidth}
+          height={imageHeight}
+        />
+      </div>
     </div>
   )
 }
