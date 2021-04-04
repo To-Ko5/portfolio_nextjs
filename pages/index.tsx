ef627b2b44ec054d5e1bf6e0a807aef4a197b173
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-04-04 09:27:35 +0900

    Add: 記事一覧を取得し、propsで渡す

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,36 @@
+import { GetStaticProps } from 'next'
+
 import Layout from '../components/layout/Layout'
 import Seo from '../components/layout/Seo'
+import PostCard from '../components/posts/PostCard'
+import { getAllPosts } from '../hooks/getAllPosts'
 
-const Home = () => {
+const Home = ({ posts }) => {
+  const postData = posts.items
+  // console.log(postData)
   return (
     <Layout>
       <Seo />
-      <main className="bg-red-100 min-h-screen">top</main>
+      <main className="">
+        {postData &&
+          postData.map((post) => (
+            <PostCard key={post.sys.id} id={post.sys.id} post={post.fields} />
+          ))}
+      </main>
     </Layout>
   )
 }
 
 export default Home
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await getAllPosts()
+  if (!response) {
+    return {
+      notFound: true
+    }
+  }
+  return {
+    props: { posts: response }
+  }
+}
