8cf2b5e25c0c33d2d3c2b64d5662ac33461b2e37
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-04-06 22:44:55 +0900

    Fix: importに変更

--- library/contentful.ts
+++ library/contentful.ts
@@ -1,4 +1,4 @@
-const contentful = require('contentful')
+import * as contentful from 'contentful'
 const config = {
   space: process.env.CTF_SPACE_ID,
   accessToken: process.env.CTF_CDA_ACCESS_TOKEN
