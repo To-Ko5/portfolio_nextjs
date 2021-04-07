6fc549c3b8a9d2df50d4a3e58bd51a58fa2c411c
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-04-07 17:21:17 +0900

    Add: markdownに対応

--- components/posts/details/PostDetailsContent.tsx
+++ components/posts/details/PostDetailsContent.tsx
@@ -1,5 +1,15 @@
-const PostDetailsContent = () => {
-  return <div></div>
+import { VFC } from 'react'
+import ReactMarkdown from 'react-markdown'
+
+import { PostDetails } from '../../../types/PostDetailsType'
+import Markdown from '../../../styles/Markdown.module.css'
+
+const PostDetailsContent: VFC<Pick<PostDetails, 'content'>> = ({ content }) => {
+  return (
+    <div className="mb-8">
+      <ReactMarkdown className={Markdown.markdown}>{content}</ReactMarkdown>
+    </div>
+  )
 }
 
 export default PostDetailsContent
