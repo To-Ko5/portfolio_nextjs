cc47082de3c652e7962cbf0e0952ed23df74c46e
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-04-07 09:50:31 +0900

    Add: titleを追加

--- components/posts/details/PostDetailsTitle.tsx
+++ components/posts/details/PostDetailsTitle.tsx
@@ -1,5 +1,16 @@
-const PostDetailsTitle = () => {
-  return <div></div>
+import { VFC } from 'react'
+import { PostDetails } from '../../../types/PostDetailsType'
+
+const PostDetailsTitle: VFC<Pick<PostDetails, 'title' | 'subTitle'>> = ({
+  title,
+  subTitle
+}) => {
+  return (
+    <div>
+      <h1 className="mb-1">{title}</h1>
+      <p>{subTitle}</p>
+    </div>
+  )
 }
 
 export default PostDetailsTitle
