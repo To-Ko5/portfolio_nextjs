27d681a6828c2b293352c85a04b1b639e60e8f31
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-04-07 17:20:43 +0900

    Add: urlのリンクを追加

--- components/posts/details/PostDetailsLink.tsx
+++ components/posts/details/PostDetailsLink.tsx
@@ -1,5 +1,19 @@
-const PostDetailsLink = () => {
-  return <div></div>
+import { VFC } from 'react'
+import { PostDetails } from '../../../types/PostDetailsType'
+
+const PostDetailsLink: VFC<Pick<PostDetails, 'url'>> = ({ url }) => {
+  return (
+    <div className="max-w-md mx-auto text-center">
+      <a
+        href={url}
+        target="_blank"
+        rel="noopener"
+        className="block w-full p-2 border border-gray-900 dark:border-white rounded cursor-pointer"
+      >
+        READ MORE
+      </a>
+    </div>
+  )
 }
 
 export default PostDetailsLink
