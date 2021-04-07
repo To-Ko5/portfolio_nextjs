b08246ddcbe87b5f3477d056bd8a7ccc4efc07af
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-04-07 10:09:12 +0900

    Add: styleを追加

--- components/posts/details/PostDetailsTitle.tsx
+++ components/posts/details/PostDetailsTitle.tsx
@@ -6,7 +6,7 @@ const PostDetailsTitle: VFC<Pick<PostDetails, 'title' | 'subTitle'>> = ({
   subTitle
 }) => {
   return (
-    <div>
+    <div className="mb-4">
       <h1 className="mb-1">{title}</h1>
       <p>{subTitle}</p>
     </div>
