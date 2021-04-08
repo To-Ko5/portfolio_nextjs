a1ee5b158668e9cd2bfdf2c29c75111c2b2c4943
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-04-09 07:52:47 +0900

    Add: categoryを全件取得する

---
+++ hooks/category/getAllCategory.ts
@@ -0,0 +1,27 @@
+import { createClient } from '../../library/contentful'
+
+const client = createClient()
+
+export const getAllCategory = async () => {
+  try {
+    const response = await client
+      .getEntries({
+        content_type: 'category'
+      })
+      .catch((error) => error)
+
+    if (response.sys.type === 'Error') {
+      throw new Error(response.message)
+    }
+
+    const categories = response.items.map((e) => {
+      return {
+        id: e.sys.id,
+        name: e.fields.name
+      }
+    })
+    return categories
+  } catch (error) {
+    console.error(error)
+  }
+}
