4961adb81863032bc5e3b8ec9c1b6823061695df
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-04-12 08:03:56 +0900

    initial commit

---
+++ pages/search/[keywords].tsx
@@ -0,0 +1,13 @@
+import Layout from '../../components/layout/Layout'
+import Seo from '../../components/layout/Seo'
+
+const SearchKeywords = () => {
+  return (
+    <Layout>
+      <Seo />
+      <div className="max-w-screen-lg mx-auto px-4"></div>
+    </Layout>
+  )
+}
+
+export default SearchKeywords
