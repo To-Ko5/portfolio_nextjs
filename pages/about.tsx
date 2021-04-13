fd909712781ccf3873feb9955608a22f9d4550c7
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-04-13 09:32:50 +0900

    initial commit

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import Layout from '../components/layout/Layout'
+import Seo from '../components/layout/Seo'
+
+const About = () => {
+  return (
+    <Layout>
+      <Seo />
+      <div>about</div>
+    </Layout>
+  )
+}
+
+export default About
