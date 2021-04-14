ee850c93160d06e597c79a0f4c41e5d7f863f691
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-04-15 07:37:34 +0900

    Add: profileの画像を表示

--- components/about/ProfileImage.tsx
+++ components/about/ProfileImage.tsx
@@ -1,5 +1,21 @@
-const ProfileImage = () => {
-  return <div></div>
+import { VFC } from 'react'
+import Image from 'next/image'
+
+import { Profile } from '../../types/ProfileType'
+
+const ProfileImage: VFC<Pick<Profile, 'image'>> = ({ image }) => {
+  return (
+    <div className="mb-4 max-h-96">
+      <Image
+        src={image.src}
+        alt={image.alt}
+        width={image.width}
+        height={image.height}
+        loading="lazy"
+        objectFit="contain"
+      />
+    </div>
+  )
 }
 
 export default ProfileImage
