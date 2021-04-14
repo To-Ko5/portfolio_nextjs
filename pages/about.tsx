bf6004f862ffd52345cc83f567bfbc57328d5b24
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-04-15 07:36:42 +0900

    Add: profileの取得を追加

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,13 +1,58 @@
+import { VFC } from 'react'
+import { GetStaticProps } from 'next'
+
 import Layout from '../components/layout/Layout'
 import Seo from '../components/layout/Seo'
+import ProfileImage from '../components/about/ProfileImage'
 
-const About = () => {
+import { getProfile } from '../hooks/about/getProfile'
+import { Profile } from '../types/ProfileType'
+
+interface Props {
+  profile: Profile
+}
+
+const About: VFC<Props> = ({ profile }) => {
   return (
     <Layout>
       <Seo />
-      <div>about</div>
+      <div className="max-w-screen-lg mx-auto px-4">
+        <ProfileImage image={profile.image} />
+      </div>
     </Layout>
   )
 }
 
 export default About
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await getProfile()
+
+  if (!response) {
+    return {
+      notFound: true
+    }
+  }
+
+  const profile: Profile = {
+    name: response.fields.name,
+    job: response.fields.job,
+    skills: response.fields.skills,
+    description: response.fields.description,
+    github: response.fields.github,
+    qiita: response.fields.qiita,
+    twitter: response.fields.twitter,
+    instagram: response.fields.instagram,
+    certifications: response.fields.certifications,
+    image: {
+      src: `https:${response.fields.profileImg.fields.file.url}`,
+      alt: response.fields.profileImg.fields.title,
+      width: response.fields.profileImg.fields.file.details.image.width,
+      height: response.fields.profileImg.fields.file.details.image.height
+    }
+  }
+
+  return {
+    props: { profile }
+  }
+}
