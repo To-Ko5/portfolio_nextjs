0d788fc28930841b9f44cda40d2cbd871670d59f
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-04-16 07:40:26 +0900

    Add: link先を組み込み

--- components/about/ProfileSnsLinks.tsx
+++ components/about/ProfileSnsLinks.tsx
@@ -3,13 +3,57 @@ import { VFC } from 'react'
 import { Profile } from '../../types/ProfileType'
 type SnsLinks = Pick<Profile, 'twitter' | 'qiita' | 'github' | 'instagram'>
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faNewspaper } from '@fortawesome/free-solid-svg-icons'
+import {
+  faTwitter,
+  faGithub,
+  faInstagram
+} from '@fortawesome/free-brands-svg-icons'
+
 const ProfileSnsLinks: VFC<SnsLinks> = ({
   twitter,
   qiita,
   github,
   instagram
 }) => {
-  return <div>{twitter && twitter}</div>
+  return (
+    <div className="mb-10">
+      <div className="flex items-center">
+        {twitter && (
+          <div className="mr-6">
+            <a href={twitter} target="_blank" rel="noopener">
+              <FontAwesomeIcon icon={faTwitter} width="24px" />
+            </a>
+          </div>
+        )}
+
+        {qiita && (
+          <div className="mr-6">
+            <a href={qiita} target="_blank" rel="noopener">
+              <FontAwesomeIcon icon={faNewspaper} width="24px" />
+            </a>
+          </div>
+        )}
+
+        {github && (
+          <div className="mr-6">
+            <a href={github} target="_blank" rel="noopener">
+              <FontAwesomeIcon icon={faGithub} width="24px" />
+            </a>
+          </div>
+        )}
+
+        {instagram && (
+          <div>
+            <a href={instagram} target="_blank" rel="noopener">
+              <FontAwesomeIcon icon={faInstagram} width="24px" />
+            </a>
+          </div>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default ProfileSnsLinks
