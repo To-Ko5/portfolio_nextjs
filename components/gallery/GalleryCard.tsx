a704e433fcceba2b3aac0386acce6c8e26376262
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-04-19 08:35:30 +0900

    Add: galleryの一覧表示を追加

--- components/gallery/GalleryCard.tsx
+++ components/gallery/GalleryCard.tsx
@@ -1,5 +1,19 @@
-const GalleryCard = () => {
-  return <div></div>
+import { VFC } from 'react'
+
+import { Gallery } from '../../types/GalleryType'
+
+const GalleryCard: VFC<Pick<Gallery, 'image'>> = ({ image }) => {
+  return (
+    <div>
+      <img
+        src={image.src}
+        alt={image.alt}
+        width={image.width}
+        height={image.height}
+        loading="lazy"
+      />
+    </div>
+  )
 }
 
 export default GalleryCard
--- pages/gallery/index.tsx
+++ pages/gallery/index.tsx
@@ -3,6 +3,7 @@ import { GetStaticProps } from 'next'
 
 import Layout from '../../components/layout/Layout'
 import Seo from '../../components/layout/Seo'
+import GalleryCard from '../../components/gallery/GalleryCard'
 
 import { GalleryTag, Gallery } from '../../types/GalleryType'
 import { getAllGalleries } from '../../hooks/gellery/getAllGalleries'
@@ -16,6 +17,13 @@ const GalleryIndex: VFC<Props> = ({ tags, galleries }) => {
   return (
     <Layout>
       <Seo />
+      <div className="max-w-screen-lg mx-auto px-4">
+        <h1 className="text-3xl font-medium text-center mb-8">Gallery</h1>
+        <div className="grid items-center grid-cols-2 gap-4 md:grid-cols-3">
+          {galleries &&
+            galleries.map((gallery) => <GalleryCard image={gallery.image} />)}
+        </div>
+      </div>
     </Layout>
   )
 }
