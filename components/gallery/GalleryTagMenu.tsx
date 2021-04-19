01f5ef4ced16516aeb54115c866771369ce74523
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-04-20 08:57:28 +0900

    Add: active時のstyleを追加

--- components/gallery/GalleryTagMenu.tsx
+++ components/gallery/GalleryTagMenu.tsx
@@ -4,12 +4,19 @@ import { GalleryTag as GalleryTagType } from '../../types/GalleryType'
 
 interface Props {
   tag: GalleryTagType
+  tagMenuId: string
   clickTabMenu: (key: string) => void
 }
-const GalleryTagMenu: VFC<Props> = ({ tag, clickTabMenu }) => {
+const GalleryTagMenu: VFC<Props> = ({ tag, tagMenuId, clickTabMenu }) => {
+  const nomalStyle =
+    'px-6 py-4 border-b-2 border-transparent focus:outline-none'
+  const activeStyle = `${nomalStyle} border-deep-blue`
+
+  const style = tag.id !== tagMenuId ? nomalStyle : activeStyle
+
   return (
     <div>
-      <button onClick={() => clickTabMenu(tag.id)} className="bg-red-100">
+      <button className={style} onClick={() => clickTabMenu(tag.id)}>
         {tag.name}
       </button>
     </div>
