5870d016cc23ebda8f4723c99ddb1e8f5542c08d
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-04-21 07:47:34 +0900

    Add: styleを追加

--- pages/gallery/index.tsx
+++ pages/gallery/index.tsx
@@ -34,7 +34,7 @@ const GalleryIndex: VFC<Props> = ({ tags, galleries }) => {
       <Seo />
       <div className="max-w-screen-lg mx-auto px-4">
         <h1 className="text-3xl font-medium text-center mb-8">Gallery</h1>
-        <div className="flex mb-4">
+        <div className="flex mb-4 bg-gray-50 dark:bg-dark-black shadow-md">
           {tagMenus &&
             tagMenus.map((tag) => (
               <GalleryTagMenu
