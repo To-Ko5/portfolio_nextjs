0fbb4536210234bb9147a21d774c42cc4bfe4c0e
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-04-26 08:30:46 +0900

    Add: scroll eventを追加

--- components/common/buttons/ReturnTopButton.tsx
+++ components/common/buttons/ReturnTopButton.tsx
@@ -1,13 +1,48 @@
+import { useEffect, useState } from 'react'
+
 const ReturnTopButton = () => {
+  const [isButtonActive, setIsButtonActive] = useState(false)
+
   const returnTop = () => {
     window.scrollTo({
       top: 0,
       behavior: 'smooth'
     })
   }
+
+  useEffect(() => {
+    window.addEventListener('scroll', scrollWindow)
+    return () => {
+      window.removeEventListener('scroll', scrollWindow)
+    }
+  }, [])
+
+  const scrollWindow = () => {
+    const top = 100
+    let scroll = 0
+    scroll = window.scrollY
+    if (top <= scroll) {
+      setIsButtonActive(true)
+    } else {
+      setIsButtonActive(false)
+    }
+  }
+
+  const normalStyle = {
+    opacity: 0,
+    transition: '0.3s',
+    pointerEvents: 'none'
+  }
+  const activeStyle = {
+    opacity: 1,
+    transition: '0.3s'
+  }
+  const style = isButtonActive ? activeStyle : normalStyle
+
   return (
     <button
-      className="fixed bottom-3.5 right-3.5 w-14 h-14 focus:outline-none  p-2 text-center rounded-full bg-gray-50 dark:bg-dark-black shadow-lg outline-none border-0"
+      className="fixed bottom-3.5 right-3.5 w-14 h-14 focus:outline-none p-2 text-center rounded-full bg-gray-50 dark:bg-dark-black shadow-lg outline-none border-0"
+      style={style}
       onClick={returnTop}
     >
       <span>
@@ -16,7 +51,6 @@ const ReturnTopButton = () => {
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
-          className=""
         >
           <path
             strokeLinecap="round"
