7a209626e33bc905d5d2f4f0bed4e591c10479ee
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-04-28 07:46:26 +0900

    Add: topへのリンクを追加

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,38 @@
+import Link from 'next/link'
+
 import Layout from '../components/layout/Layout'
+import Seo from '../components/layout/Seo'
 
 const Page404 = () => {
   return (
     <Layout>
-      <main className="">404</main>
+      <Seo pageTitle={'ページが見つかりません'} />
+      <div className="max-w-screen-lg mx-auto px-4">
+        <h1 className="text-4xl font-medium text-center mb-16">
+          404 <br /> Page Not Found
+        </h1>
+        <div>
+          <Link href="/">
+            <a className="p-2 border border-gray-400 rounded text-gray-400 hover:bg-opacity-20 hover:bg-gray-400 transition-colors">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="4-5 w-4 mr-1 mb-1 inline"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M10 19l-7-7m0 0l7-7m-7 7h18"
+                />
+              </svg>
+              TOPへ戻る
+            </a>
+          </Link>
+        </div>
+      </div>
     </Layout>
   )
 }
