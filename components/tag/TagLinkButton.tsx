039526be601500124d1f552f345829f106eadb81
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-05-07 08:47:36 +0900

    Add: hover時のstyleを追加

--- components/tag/TagLinkButton.tsx
+++ components/tag/TagLinkButton.tsx
@@ -6,7 +6,7 @@ const TagLinkButton: VFC<PostTag> = ({ id, name }) => {
   return (
     <div className="text-center">
       <Link href={`/tag/${id}`}>
-        <a className="block w-full p-2 border border-gray-900 dark:border-white rounded cursor-pointer">
+        <a className="block w-full p-2 border border-gray-900 dark:border-white rounded hover:bg-opacity-20 hover:bg-gray-400 dark:hover:bg-opacity-20 dark:hover:bg-gray-400 transition-colors cursor-pointer">
           {name}
         </a>
       </Link>
