5a12fe5536e91d8022d3f18670e5e7a7fc9cc428
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-05-13 07:48:43 +0900

    initial commit

---
+++ pages/contact.tsx
@@ -0,0 +1,13 @@
+import Layout from '../components/layout/Layout'
+import Seo from '../components/layout/Seo'
+
+const contact = () => {
+  return (
+    <Layout>
+      <Seo pageTitle={'ページが見つかりません'} />
+      <div className="max-w-2xl mx-auto px-4"></div>
+    </Layout>
+  )
+}
+
+export default contact
