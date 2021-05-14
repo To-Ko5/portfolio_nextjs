5736d3687059f9883e78016743b1a1f7a0b21f97
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-05-15 08:22:02 +0900

    Add: お問い合わせのlinkを追加

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -5,7 +5,18 @@ const contact = () => {
   return (
     <Layout>
       <Seo pageTitle={'Contact'} />
-      <div className="max-w-2xl mx-auto px-4"></div>
+      <div className="max-w-2xl mx-auto px-4">
+        <div className="text-center">
+          <a
+            href={process.env.NEXT_PUBLIC_PORTFOLIO_CONTACT_URL}
+            target="_blank"
+            rel="noopener"
+            className="block w-full p-2 border border-gray-900 dark:border-white rounded hover:bg-opacity-20 hover:bg-gray-400 dark:hover:bg-opacity-20 dark:hover:bg-gray-400 transition-colors cursor-pointer"
+          >
+            お問い合わせはこちらから
+          </a>
+        </div>
+      </div>
     </Layout>
   )
 }
